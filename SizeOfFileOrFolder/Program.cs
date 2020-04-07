cde7bdd1c0fb1957c05a0cba7cad67feb1a5f771
PopoviciSerban <serbangaming24@gmail.com>
2020-04-07 23:21:18 +0300

    Add project files.

---
+++ SizeOfFileOrFolder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SizeOfFileOrFolder
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fileName = @"C:\Users\Serban\Documents\smk.txt";
+
+            FileInfo f = new FileInfo(fileName);
+
+            Console.WriteLine("The size of {0} is {1} bytes.", f.Name, f.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
